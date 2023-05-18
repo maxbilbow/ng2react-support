38219cc8f486f34b3ff506858d07c26a153381b3
Max Bilbow <max.bilbow@uk.bosch.com>
2023-05-18 09:30:13 +0100

    Prevent use of bindings when element contains no props

--- src/test/WrapperOptions.tsx
+++ src/test/WrapperOptions.tsx
@@ -1,22 +1,44 @@
-import React from 'react'
-import { angularize } from '../lib/index'
+/* eslint-disable @typescript-eslint/naming-convention */
 
-type MyProps = {
-    value: string
-    value2: string
-    value3: string | undefined
-    value4?: boolean
-    onValueChange: (value: string) => void
-}
+import {angularize} from "../lib";
+import * as React from "react";
 
-const MyFC = (props: MyProps) => {
-    return <></>
-}
+type Props = {
+    value: string,
+    value2: bigint,
+    value3: string,
+    value4: Algorithm,
+    value5: BarProp,
+    onValueChange: (newValue: string) => void,
+};
+const MyElement = (props: Props) => {
+    return <>{props}</>;
+};
+const NoProps = () => <></>;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const EmptyProps = (props: NonNullable<unknown>) => <></>;
 
-angularize(MyFC, {
-    name: 'myFC',
+angularize(MyElement, {
+    name: "",
     bindings: {
-        value: ['=', 'onValueChange'],
-        value2: '<',
-    },
-})
+        value: ["=", "onValueChange"],
+        value2: "=", // not allowed,
+        value3: "<",
+        value4: ["<", "notAllowed"],
+        value5: "@"
+    }
+});
+
+angularize(NoProps, {
+    name: "",
+    // Not allowed (no bindings)
+    bindings: {},
+    require: {}
+});
+
+angularize(EmptyProps, {
+    name: "",
+    // Not allowed (no bindings)
+    bindings: {},
+    require: {}
+});
