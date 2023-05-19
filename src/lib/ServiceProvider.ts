f9b7972e0d1ff5e79f43c93a211a4819f2c37907
maxbilbow <maxbilbow@gmail.com>
2023-05-19 10:53:40 +0100

    align

--- src/lib/ServiceProvider.ts
+++ src/lib/ServiceProvider.ts
@@ -13,34 +13,34 @@ export function getService<T, TName extends string>(name: ServiceName<TName>): T
  * @deprecated Do not inject Angular services into react code. Instead, find an alternative or create a wrapper
  */
 export function getService<T, TName extends string>(
-    name: AngularServiceName<TName>,
-    options: { allowUnsafeAngularService: true }
+  name: AngularServiceName<TName>,
+  options: { allowUnsafeAngularService: true }
 ): T
 export function getService<T>(name: string, { allowUnsafeAngularService = false } = {}): T {
-    if (name.startsWith('$') && !allowUnsafeAngularService) {
-        throw Error(
-            // eslint-disable-next-line max-len
-            `${name} looks like an angular service. Do not try to retrieve these outside of the angular framework. Create an abstracted wrapper instead.`
-        )
-    }
+  if (name.startsWith('$') && !allowUnsafeAngularService) {
+    throw Error(
+      // eslint-disable-next-line max-len
+      `${name} looks like an angular service. Do not try to retrieve these outside of the angular framework. Create an abstracted wrapper instead.`
+    )
+  }
 
-    const service = findService(name)
-    if (!service) {
-        throw Error(`${name} not registered`)
-    }
-    return service as T
+  const service = findService(name)
+  if (!service) {
+    throw Error(`${name} not registered`)
+  }
+  return service as T
 }
 
 function findService(name: string) {
-    if (globalServices.has(name) || typeof window === 'undefined' || typeof window.angular === 'undefined') {
-        return globalServices.get(name)
-    }
-    return getAngularService(name)
+  if (globalServices.has(name) || typeof window === 'undefined' || typeof window.angular === 'undefined') {
+    return globalServices.get(name)
+  }
+  return getAngularService(name)
 }
 
 /**
  * Method for retrieving an Angular service
  */
 export function getAngularService<T>(name: string): T {
-    return window.angular.element(document.body).injector().get(name)
+  return window.angular.element(document.body).injector().get(name)
 }
--- src/lib/wrappers/angularizeDirective.ts
+++ src/lib/wrappers/angularizeDirective.ts
@@ -4,75 +4,75 @@ import * as ReactDOMClient from 'react-dom/client'
 import type { BindingType, FC, FCProps, PropsCallbackName, WrapperOptions } from './WrapperOptions'
 
 type DirectiveOptions<T extends FC> = {
-    scopeBindings?: Record<string, BindingType>
-    onChangeHandlers?: [keyof FCProps<T>, PropsCallbackName<T>][]
-    require?: WrapperOptions<T>['require']
-    replace?: boolean
+  scopeBindings?: Record<string, BindingType>
+  onChangeHandlers?: [keyof FCProps<T>, PropsCallbackName<T>][]
+  require?: WrapperOptions<T>['require']
+  replace?: boolean
 }
 
 const logger = console
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export function angularizeDirective<T extends FC>(
-    Component: T,
-    directiveName: string,
-    angularApp: IModule,
-    options: DirectiveOptions<T>
+  Component: T,
+  directiveName: string,
+  angularApp: IModule,
+  options: DirectiveOptions<T>
 ) {
-    if (typeof window === 'undefined' || typeof angularApp === 'undefined') {
-        return
-    }
-    const { scopeBindings = {}, require: requiredControllers = {}, replace = false, onChangeHandlers = [] } = options
-    angularApp.directive(directiveName, () => ({
-        scope: scopeBindings,
-        replace,
-        require: requiredControllers,
-        /**
-         * @this {import("angular").IDirective & Record<string, any>}
-         * @param {import("angular").IScope & Record<string, any>} scope
-         * @param $element
-         * @param attrs
-         * @param {{[key: string]: import("angular").IController}} [ctrl]
-         */
-        link: function (this: IDirectiveLinkFn, scope: IScope & Record<string, unknown>, $element, attrs, ctrl) {
-            // Add $scope
-            Object.assign(scope, ctrl)
+  if (typeof window === 'undefined' || typeof angularApp === 'undefined') {
+    return
+  }
+  const { scopeBindings = {}, require: requiredControllers = {}, replace = false, onChangeHandlers = [] } = options
+  angularApp.directive(directiveName, () => ({
+    scope: scopeBindings,
+    replace,
+    require: requiredControllers,
+    /**
+     * @this {import("angular").IDirective & Record<string, any>}
+     * @param {import("angular").IScope & Record<string, any>} scope
+     * @param $element
+     * @param attrs
+     * @param {{[key: string]: import("angular").IController}} [ctrl]
+     */
+    link: function (this: IDirectiveLinkFn, scope: IScope & Record<string, unknown>, $element, attrs, ctrl) {
+      // Add $scope
+      Object.assign(scope, ctrl)
 
-            // Tech Debt: Why is $scope added here?
-            scope.$scope = scope
-            const root = ReactDOMClient.createRoot($element[0])
+      // Tech Debt: Why is $scope added here?
+      scope.$scope = scope
+      const root = ReactDOMClient.createRoot($element[0])
 
-            scope.$on('$destroy', () => root.unmount())
+      scope.$on('$destroy', () => root.unmount())
 
-            // Watch for any changes in bindings, then rerender
-            const keys = Object.keys(scopeBindings).filter((bindingKey) => {
-                if (/^data[A-Z]/.test(bindingKey)) {
-                    logger.warn(
-                        // eslint-disable-next-line max-len
-                        `"${bindingKey}" binding for ${directiveName} directive will be undefined because AngularJS ignores attributes starting with data-`
-                    )
-                    return false
-                }
-                return !scopeBindings[bindingKey].includes('&')
-            })
+      // Watch for any changes in bindings, then rerender
+      const keys = Object.keys(scopeBindings).filter((bindingKey) => {
+        if (/^data[A-Z]/.test(bindingKey)) {
+          logger.warn(
+            // eslint-disable-next-line max-len
+            `"${bindingKey}" binding for ${directiveName} directive will be undefined because AngularJS ignores attributes starting with data-`
+          )
+          return false
+        }
+        return !scopeBindings[bindingKey].includes('&')
+      })
 
-            scope.$watchGroup(keys, () => renderReact())
+      scope.$watchGroup(keys, () => renderReact())
 
-            for (const [propName, callbackName] of onChangeHandlers) {
-                logger.debug(`Adding onChange handler for ${propName as string} to ${callbackName}`)
-                scope[callbackName as string] = (newValue: unknown) => {
-                    scope.$applyAsync(() => {
-                        scope[propName as string] = newValue
-                    })
-                }
-            }
+      for (const [propName, callbackName] of onChangeHandlers) {
+        logger.debug(`Adding onChange handler for ${propName as string} to ${callbackName}`)
+        scope[callbackName as string] = (newValue: unknown) => {
+          scope.$applyAsync(() => {
+            scope[propName as string] = newValue
+          })
+        }
+      }
 
-            // First render - needed?
-            renderReact()
+      // First render - needed?
+      renderReact()
 
-            function renderReact() {
-                root.render(React.createElement(Component, scope))
-            }
-        },
-    }))
+      function renderReact() {
+        root.render(React.createElement(Component, scope))
+      }
+    },
+  }))
 }
